e46bc43ac79dec41454d7b6a8774f541ee11049b
Mateen-Amad5 <mateenahmad9235@gmail.com>
2024-07-07 23:28:25 +0500

    task5 done

--- edit.js
+++ edit.js
@@ -1,38 +1,97 @@
+let company = document.getElementById("company");
+let startDate = document.getElementById("start-date");
+let endDate = document.getElementById("end-date");
+let description = document.getElementById("description");
+
+let isEdit = false;
+
 function showExperienceForm() {
   document.getElementById("experience-form").style.display = "block";
 }
+
 document.getElementById("experience-form").addEventListener("submit", function (event) {
     event.preventDefault();
 
-    let company = document.getElementById("company");
-    let startDate = document.getElementById("start-date");
-    let endDate = document.getElementById("end-date");
-    let description = document.getElementById("description");
-    
-    if (
-      company.value &&startDate.value &&endDate.value &&description.value ) {
-      let experiences = JSON.parse(localStorage.getItem("experience")) || [];
+    let experiences = JSON.parse(localStorage.getItem("experience")) || [];
+
+    let id = Date.now().toString(36) + Math.random().toString(36).substr(2, 9);
+
+    if (isEdit) {
+      let newExperience = {
+        id: isEdit,
+        company: company.value,
+        startDate: startDate.value,
+        endDate: endDate.value,
+        description: description.value,
+      };
+
+      let newExperiences = experiences.map((exp) => {
+        if (exp.id == isEdit) {
+          return newExperience;
+        } else {
+          return exp;
+        }
+      });
+
+      localStorage.setItem("experience", JSON.stringify(newExperiences));
+      isEdit = false;
+    } else if (
+      company.value &&startDate.value &&endDate.value &&description.value
+    ) {
       experiences.push({
+        id: id,
         company: company.value,
         startDate: startDate.value,
         endDate: endDate.value,
         description: description.value,
       });
-      document.getElementById("experience-form").reset();
+
       localStorage.setItem("experience", JSON.stringify(experiences));
-      document.getElementById("experience-form").style.display = "none";
-      loadExperiences();
     }
+
+    document.getElementById("experience-form").reset();
+    document.getElementById("experience-form").style.display = "none";
+    loadExperiences();
   });
+function editExperience(id) {
+  showExperienceForm();
+  let experiences = JSON.parse(localStorage.getItem("experience")) || [];
+  let experience = experiences.find((exp) => exp.id == id);
+  isEdit = experience.id;
+  if (experience) {
+    company.value = experience.company;
+    description.value = experience.description;
+    startDate.value = experience.startDate;
+    endDate.value = experience.endDate;
+  }
+}
+
+function deleteExperience(id) {
+  let experiences = JSON.parse(localStorage.getItem("experience")) || [];
+  if (experiences.length > 0) {
+    let newExperience = experiences.filter((exp) => {
+      if (exp.id !== id) {
+        return exp;
+      }
+    });
+    localStorage.setItem("experience", JSON.stringify(newExperience));
+    loadExperiences();
+  }
+}
+
 function loadExperiences() {
   let experiences = JSON.parse(localStorage.getItem("experience")) || [];
   const experienceEntries = document.getElementById("experience-entries");
   experienceEntries.innerHTML = "";
 
   experiences.forEach((exp) => {
-    let experienceEntry = document.createElement("div");
+    const experienceEntry = document.createElement("div");
     experienceEntry.classList.add("experience-entry");
     experienceEntry.innerHTML = `
+            <div class="button-container">
+            <button class="edit-button" onclick="editExperience('${exp.id}')">Edit</button>
+            <button class="delete-button"  onclick="deleteExperience('${exp.id}')">Delete</button>
+            </div>
             <h3>Company Name: ${exp.company}</h3>
             <p>Start Date: ${exp.startDate}</p>
             <p>End Date: ${exp.endDate}</p>
@@ -41,4 +100,5 @@ function loadExperiences() {
     experienceEntries.appendChild(experienceEntry);
   });
 }
+
 loadExperiences();
--- javascript.js
+++ javascript.js
@@ -1,20 +1,42 @@
-window.addEventListener("load", function () {
-  function displayExperiences() {
+
+let filterExperienceField = document.getElementById("filterExperience");
+
+filterExperienceField.addEventListener("input", (e) => {
+  let experiences = JSON.parse(localStorage.getItem("experience")) || [];
+
+  let newExperience = experiences.filter((exp) => {
+    if (exp.company.includes(e.target.value)) {
+      return exp;
+    }
+  });
+
+  if (e.target.value !== "") {
+    loadExperiences(newExperience);
+  } else {
     let experiences = JSON.parse(localStorage.getItem("experience")) || [];
-    experiences.forEach(function (exp) {
-      const experienceEntry = document.createElement("div");
-      experienceEntry.classList.add("experience-entry");
-      experienceEntry.innerHTML = `
+    loadExperiences(experiences);
+  }
+});
+
+window.addEventListener("load", function () {
+  let experiences = JSON.parse(localStorage.getItem("experience")) || [];
+
+  loadExperiences(experiences);
+});
+
+const loadExperiences = (experiences) => {
+  const experienceEntries = document.getElementById("experience-entries");
+  experienceEntries.innerHTML = "";
+
+  experiences.forEach(function (exp) {
+    const experienceEntry = document.createElement("div");
+    experienceEntry.classList.add("experience-entry");
+    experienceEntry.innerHTML = `
             <h3>Company Name: ${exp.company}</h3>
             <p>Start Date: ${exp.startDate}</p>
-            <p>End Date: ${exp.endDate}</p>
+            <p>Start Date: ${exp.endDate}</p>
             <p>Description: ${exp.description}</p>
         `;
-      document
-        .getElementById("experience-entries")
-        .appendChild(experienceEntry);
-    });
-  }
-
-  displayExperiences();
-});
+    experienceEntries.appendChild(experienceEntry);
+  });
+};
